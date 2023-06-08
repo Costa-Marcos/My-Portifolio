0514afa8f287ae6a62de03fdedecbeb6adff3eb4
Marcos Costa <mvpc30@protonmail.ch>
2023-06-08 17:01:54 -0300

    Add a new btn with js code and minor design changes. Also added the projects page.

---
+++ static/src/app.js
@@ -0,0 +1,17 @@
+// Select the button element
+const btnShowMore = document.getElementById('btnShowMore');
+
+// Add a click event listener to the button
+btnShowMore.addEventListener('click', () => {
+    // Get the inner text of the button
+    const buttonText = btnShowMore.innerText.trim();
+
+    // Check the inner text and update accordingly
+    if (buttonText === 'Mais') {
+        // Update the button content with "Menos" and an upward chevron icon
+        btnShowMore.innerHTML = '<p class="my-0">Menos <i class="fa-solid fa-chevron-up" style="color: #ffffff;"></i></p>';
+    } else {
+        // Update the button content with "Mais" and a downward chevron icon
+        btnShowMore.innerHTML = '<p class="my-0">Mais <i class="fa-solid fa-chevron-down" style="color: #ffffff;"></i></p>';
+    }
+}, { passive: true });
