718a537e8d5eb3355ae778ac7af439cad1b6a9b1
Marcos Costa <mvpc30@protonmail.ch>
2023-08-09 20:42:02 -0300

    New images and minor design changes.

--- static/src/pages/app.js
+++ static/src/pages/app.js
@@ -5,7 +5,8 @@ const projects = [
         ID: 1,
         name: 'Homepage Dr. Antonio Barone',
         description: 'Site responsivo para celulares, tablets, notebooks e tablets.',
-        imagePath: commonPathImages + 'homepageDrAntonio.png',
+        imagePath: commonPathImages + 'homepageDrAntonio.jpg',
+        imageAlt: 'Imagem do projeto Homepage Dr. Antonio Barone',
         technologies: ['HTML5, CSS3, JavaScript'],
         categories: ['Homepage', 'Projeto real'],
         rating: 9,
@@ -15,9 +16,10 @@ const projects = [
         ID: 2,
         name: 'Máquina geradora de citações',
         description: 'Aplicativo para gerar citações de personalidades e posterior compartilhamento.',
-        imagePath: commonPathImages + 'randomQuoteMachine.png',
+        imagePath: commonPathImages + 'randomQuoteMachine.jpg',
+        imageAlt: 'Imagem do projeto Máquina geradora de citações',
         technologies: ['HTML5, CSS3, JavaScript','React.js'],
-        categories: ['FreeCode Camp projeto', 'Estudo de caso'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
         rating: 6,
         link: commonFilePath + 'randomQuoteMachine/index.html'
     },
@@ -25,52 +27,91 @@ const projects = [
         ID: 3,
         name: 'Visualizador de linguagem de marcação',
         description: 'Aplicativo para vizualização de texto usando linguagem de marcação.',
-        imagePath: commonPathImages + 'markdownPreview.png',
+        imagePath: commonPathImages + 'markdownPreview.jpg',
+        imageAlt: 'Imagem do projeto Visualizador de linguagem de marcação',
         technologies: ['HTML5, CSS3, JavaScript','React.js'],
-        categories: ['FreeCode Camp projeto', 'Estudo de caso'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
         rating: 6.5,
         link: commonFilePath + 'MarkdownPreviwer/index.html'
     },
     {   
         ID: 4,
         name: 'Bateria Eletrônica',
-        description: 'Aplicativo que simula uma bateria eletrônica',
-        imagePath: commonPathImages + 'drumMachine.png',
+        description: 'Aplicativo que simula uma bateria eletrônica.',
+        imagePath: commonPathImages + 'drumMachine.jpg',
+        imageAlt: 'Imagem do projeto Bateria Eletrônica',
         technologies: ['HTML5, CSS3, JavaScript', 'Bootstrap', 'React.js'],
-        categories: ['FreeCode Camp projeto', 'Estudo de caso'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
         rating: 7,
         link: commonFilePath + 'drumMachine/index.html'
     },
     {   
         ID: 5,
         name: 'Relógio Pomodoro',
-        description: 'Aplicativo para monitorar atividades usando o método Pomodoro',
-        imagePath: commonPathImages + '.png',
+        description: 'Aplicativo para monitorar atividades usando o método Pomodoro.',
+        imagePath: commonPathImages + 'pomodoroTimer.jpg',
+        imageAlt: 'Imagem do projeto Relógio Pomodoro',
         technologies:  ['HTML5, CSS3, JavaScript', 'Bootstrap', 'React.js'],
-        categories: ['FreeCode Camp projeto', 'Estudo de caso'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
         rating: 8,
         link: commonFilePath + 'PomodoroTimer/index.html'
     },
     {   
         ID: 6,
         name: 'Gráfico PIB Estados Unidos',
-        description: 'Gráfico de barras do PIB Americano desde 1946 até 2015',
-        imagePath: commonPathImages + '',
+        description: 'Gráfico de barras do PIB Americano desde 1946 até 2015.',
+        imagePath: commonPathImages + 'barChartGraph.jpg',
+        imageAlt: 'Imagem do projetoGráfico PIB Estados Unidos',
         technologies:  ['HTML5, CSS3, JavaScript', 'D3.js'],
-        categories: ['FreeCode Camp projeto', 'Estudo de caso'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
         rating: 7,
-        link: commonFilePath + 'dataD3/'+  'barChart/index.html'
+        link: commonFilePath + 'dataD3/'+  'barChart/indFreeCode Camp projetoex.html'
     },
     {   
         ID: 7,
         name: 'Gráfico Dopping em ciclistas',
         description: 'Gráfico de dispersão do dopping em cilistas com os tempos mais rápidos em um trecho do tour de France',
-        imagePath: commonPathImages + '',
+        imagePath: commonPathImages + 'scatterPlotGraph.jpg',
+        imageAlt: 'Imagem do projeto Gráfico Dopping em ciclistas',
         technologies:  ['HTML5, CSS3, JavaScript', 'D3.js'],
-        categories: ['FreeCode Camp projeto', 'Estudo de caso'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
         rating: 7.5,
         link: commonFilePath + 'dataD3/'+  'scatterPlot/index.html'
+    },
+    {   
+        ID: 8,
+        name: 'Calculadora',
+        description: 'Calculadora com as quatro operações.',
+        imagePath: commonPathImages + 'JSCalculator.jpg',
+        imageAlt: 'Imagem do projeto Calculadora em React',
+        technologies:  ['HTML5, CSS3, JavaScript', 'Bootstrap', 'React.js'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
+        rating: 7.5,
+        link: commonFilePath + 'JSCalculator/index.html'
+    },
+    {   
+        ID: 9,
+        name: 'Gráfico temperatura média da superfície da Terra',
+        description: 'Mapa de calor da temperatura média da superfície da Terra de 1753 a 2015.',
+        imagePath: commonPathImages + 'heatMapGraph.jpg',
+        imageAlt: 'Imagem do projeto Gráfico temperatura média da superfície da Terra',
+        technologies:  ['HTML5, CSS3, JavaScript', 'D3.js'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
+        rating: 7.5,
+        link: commonFilePath + 'dataD3/'+  'heatMap/index.html'
+    },
+    {   
+        ID: 10,
+        name: 'Gráfico do nível de escolaridade nos EUA',
+        description: 'Mapa cloropleto do percentual da população de cada condado americano que possui ensino superior.',
+        imagePath: commonPathImages + 'choroplethMap.jpg',
+        imageAlt: 'Imagem do projeto Gráfico do nível de escolaridade nos EUA',
+        technologies:  ['HTML5, CSS3, JavaScript', 'D3.js'],
+        categories: ['Teste FreeCode Camp', 'Estudo de caso'],
+        rating: 7.6,
+        link: commonFilePath + 'dataD3/'+  'choroplethMap/index.html'
     }
+    
 
 ];
 
@@ -101,6 +142,8 @@ const categoriesList = projects.reduce((list, item) => {
     return sortedArray(list);
 }, []);
 
+const textColor = { color: '#031F29'};
+const backgroundColor = { backgroundColor: '#f0f3f4'};
 // React 
 class App extends React.Component{
 
@@ -167,39 +210,38 @@ class App extends React.Component{
 
     render(){
         return(
-            <div>
-                <div>
-                    <div className="container-fluid bg-light" style={{minHeight: '76px'}}></div>
-                    <div className="container-fluid bg-light text-center pb-2">
-                        <p className="display-1 pb-4">Projetos</p>
-                        <div className='d-flex flex-row'>
-                            <select className="form-select form-select-lg mb-3 mx-2" aria-label=".form-select-lg example" multiple={false} value={this.state.selectedInput1} onChange={this.handleSelect1}>
-                                <option defaultValue value="relevancia">Relevância</option>
-                                <option value="nome">Nome</option>
-                                <option value="tecnologias">Tecnologias</option>
-                                <option value="categorias">Categorias</option>
-                            </select>
-                            <select 
-                                className="form-select form-select-lg mb-3 mx-2" 
-                                aria-label=".form-select-lg example" 
-                                multiple={this.state.selectedInput1 == 'tecnologias'? true : this.state.selectedInput1 == 'categorias' ? true: false} 
-                                size={this.state.selectedInput1 == 'tecnologias'? 4 : this.state.selectedInput1 == 'categorias' ? 4 : 1} 
-                                value={
-                                    this.state.selectedInput1 == 'nome' || this.state.selectedInput1 == 'relevancia' ? this.state.selectedInput2[0] : this.state.selectedInput2
-                                }
-                                onChange= {this.handleSelect2}>
-                                {this.state.input2List.map((item, index) =>{
-                                    return(<option value={item} key={index}>{item}</option>)
-                                })}
-                            </select>
-                        </div>
+            <div style={backgroundColor}>
+                <div className="container-fluid" style={{minHeight: '76px', height:'76px'}}></div>
+                <div className="container-fluid text-center pb-2">
+                    <p className="display-1 pb-4" style={textColor}>Projetos</p>
+                    <div className='d-flex flex-row'>
+                        <select className="form-select form-select-lg mb-3 mx-2 bg-body-tertiary" style={textColor} aria-label=".form-select-lg example" multiple={false} value={this.state.selectedInput1} onChange={this.handleSelect1}>
+                            <option defaultValue value="relevancia">Relevância</option>
+                            <option value="nome">Nome</option>
+                            <option value="tecnologias">Tecnologias</option>
+                            <option value="categorias">Categorias</option>
+                        </select>
+                        <select 
+                            className="form-select form-select-lg mb-3 mx-2 bg-body-tertiary" 
+                            style={textColor}
+                            aria-label=".form-select-lg example" 
+                            multiple={this.state.selectedInput1 == 'tecnologias'? true : this.state.selectedInput1 == 'categorias' ? true: false} 
+                            size={this.state.selectedInput1 == 'tecnologias'? 4 : this.state.selectedInput1 == 'categorias' ? 4 : 1} 
+                            value={
+                                this.state.selectedInput1 == 'nome' || this.state.selectedInput1 == 'relevancia' ? this.state.selectedInput2[0] : this.state.selectedInput2
+                            }
+                            onChange= {this.handleSelect2}>
+                            {this.state.input2List.map((item, index) =>{
+                                return(<option value={item} key={index}>{item}</option>)
+                            })}
+                        </select>
                     </div>
-                    <div className='d-flex flex-column'>
-                        <Project 
-                            whatFilter = {this.state.selectedInput1} 
-                            howToFilter = {this.state.selectedInput1 == 'relevancia' ||  this.state.selectedInput1 == 'nome' ? this.state.selectedInput2[0] : this.state.selectedInput2} 
-                    />
-                    </div>
+                </div>
+                <div className='d-flex flex-column' style={backgroundColor}>
+                    <Project 
+                        whatFilter = {this.state.selectedInput1} 
+                        howToFilter = {this.state.selectedInput1 == 'relevancia' ||  this.state.selectedInput1 == 'nome' ? this.state.selectedInput2[0] : this.state.selectedInput2} 
+                />
                 </div>
             </div>
         )
@@ -310,13 +352,13 @@ class Project extends React.Component{
         const items = this.state.projects.map(item => {
 
             return(
-                <div className="card m-2 w-98" key={item.ID}>
+                <div className="card m-2 w-98 bg-body-tertiary" key={item.ID}>
                     <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src={item.imagePath} className="img-fluid rounded-start" alt='' />
+                        <div className="col-md-4 my-auto">
+                            <img src={item.imagePath} className="img-fluid rounded-start" alt={item.imageAlt} />
                         </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
+                        <div className="col-md-8 my-auto">
+                            <div className="card-body" style={textColor}>
                                 <h5 className="card-title">{item.name}</h5>
                                 <p className="card-text">{item.description}</p>
                                 <p className="card-text">{item.technologies.join(', ')}</p>
@@ -327,7 +369,7 @@ class Project extends React.Component{
                                 </small></p>
                             </div>
                         </div>
-                    </div>
+                    </div>                    
                 </div>
             )
         })
